5adc84b5948808a5d5dd43b3c708b7cc6d64f85b
Martin <martingakuya@gmail.com>
2023-07-28 21:20:45 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+// Function to fetch the books from the API and render them on the webpage
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const url = 'https://anapioficeandfire.com/api/books';
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error:', error));
 }
 
+// Function to render the books on the webpage
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = ""; // Clear the list before rendering
+
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const bookTitle = document.createElement('li');
+    bookTitle.innerText = book.name;
+    bookList.appendChild(bookTitle);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+// Call fetchBooks() when index.html is loaded
+window.addEventListener('DOMContentLoaded', fetchBooks);
+
+// Assuming I have Chai and Chai Spies set up
+const expect = chai.expect;
+
+describe('Test for fetchBooks()', () => {
+  it('should render book titles into the DOM by passing a JSON object to renderBooks()', async () => {
+    // Arrange: Spy on the actual renderBooks function
+    const renderBooksSpy = chai.spy.on(window, 'renderBooks');
+
+    // Act: Call fetchBooks()
+    await fetchBooks();
+
+    // Assert: Check if renderBooks() has been called
+    expect(renderBooksSpy).to.have.been.called();
+  });
 });
+
